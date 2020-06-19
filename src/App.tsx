912cefe195c13038a3107e89cc18fd205cabe011
AndersonCMelo <andersonmelo13@gmail.com>
2020-06-19 00:20:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlocalStyles from './styles/globalStyles';
+
+function App() {
+  return (
+    <>
+      <Layout />
+
+      <GlocalStyles />
+    </>
+  );
+}
+
+export default App;
